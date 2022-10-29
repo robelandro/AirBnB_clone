12c17f71cb5d470e76fa0ed0545114d2c9f178e3
Robel Pop <robelpopandro@gmail.com>
2022-10-29 11:53:05 +0300

    Unitest Addvance Test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,62 +1,157 @@
 #!/usr/bin/python3
-'''Tests for BaseModel class'''
-
+"""
+Unitest Class BaseModel
+"""
 import unittest
-from models.base_model import BaseModel
+from models.base_model import BaseModel, __doc__ as mrdoc
+import inspect
+import models
+from datetime import datetime as datetime
 
-class TestBase_Model(unittest.TestCase):
-    '''start tests'''
 
-    def setUp(self):
-        '''Object created from a class'''
-        self.my_object = BaseModel()
+class TestBaseModel(unittest.TestCase):
+    """
+    Unitest for testing
+    """
+    def test_module_docstring(self):
+        """
+        Tests docstring for module
+        """
+        self.assertTrue(len(mrdoc) > 20)
 
-    def test_is_an_instance(self):
-        '''check if my_object is an instance of BaseModel'''
-        self.assertIsInstance(self.my_object, BaseModel)
+    def test_class_docstring(self):
+        """
+        Tests docstring for class
+        """
+        self.assertTrue(len(BaseModel.__doc__) > 20)
 
-    def test_id(self):
-        '''test if the id of two instances are different'''
-        instance1 = BaseModel()
-        instance2 = BaseModel()
-        self.assertNotEqual(instance1.id, instance2.id)
+    def test_docstring_for_test(self):
+        """
+        Tests docstring for this test
+        """
+        self.assertTrue(len(__doc__) > 20)
 
-    def test_str(self):
-        '''check if the output of str is in the specified format'''
-        _dict = self.my_object.__dict__
-        string1 = "[BaseModel] ({}) {}".format(self.my_object.id, _dict)
-        string2 = str(self.my_object)
-        self.assertEqual(string1, string2)
+    def test_docstring_class_test(self):
+        """
+        Tests dosctring for class TestBaseModel
+        """
+        self.assertTrue(len(TestBaseModel.__doc__) > 20)
 
-    def test_save(self):
-        '''check if the attribute updated_at (date) is updated for
-        the same object with the current date'''
-        first_updated = self.my_object.updated_at
-        self.my_object.save()
-        second_updated = self.my_object.updated_at
-        self.assertNotEqual(first_updated, second_updated)
+    def test_docstring_methods(self):
+        """
+        Tests docstring for all methods in TestBaseModel class
+        """
+        methods = inspect.getmembers(TestBaseModel, predicate=inspect.ismethod)
+        for name, func in methods:
+            self.assertTrue(len(func.__doc__) > 20)
+
+    def test_base_init(self):
+        """
+        Testing a class BaseModel
+        """
+        instance = BaseModel()
+        self.assertIsInstance(instance, BaseModel)
+        self.assertTrue(issubclass(type(instance), BaseModel))
+        self.assertIs(type(instance), BaseModel)
+
+        instance.name = "Holberton"
+        instance.my_number = 89
+        self.assertEqual(instance.name, "Holberton")
+        self.assertEqual(instance.my_number, 89)
+        """
+        at_class = {
+            "id": str,
+            "created_at": datetime
+            "updated_at": datetime
+            "name": str
+            "my_number": int
+        }
+        """
+
+    def test_none(self):
+        """Check if a new instance is not none"""
+        bm1 = BaseModel()
+        self.assertIsNotNone(bm1)
+
+    def test_uuid(self):
+        """Check ids in the created instances"""
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertTrue(hasattr(bm1, "id"))
+        self.assertNotEqual(bm1.id, bm2.id)
+
+    def test_created_at(self):
+        """Check if the instance has created_at Atttibute"""
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertTrue(bm1, "created_at")
+        self.assertTrue(bm2, "created_at")
+
+    def test_updated_at(self):
+        """Check if the instance has created_at Atttibute"""
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertTrue(bm1, "updated_at")
+        self.assertTrue(bm2, "updated_at")
+
+    def test__str__(self):
+        """Check the string of an created instance"""
+        bm1 = BaseModel()
+        printed = "[{}] ({}) {}".format(
+            bm1.__class__.__name__, bm1.id, bm1.__dict__)
+        self.assertEqual(str(bm1), printed)
 
     def test_to_dict(self):
-        '''check if to_dict returns a dictionary, if add a class
-        key with class name of the object and if updated_at and
-        created_at are converted to string object in ISO format.'''
-        my_dict_model = self.my_object.to_dict()
-        self.assertIsInstance(my_dict_model, dict)
-        for key, value in my_dict_model.items():
-            flag = 0
-            if my_dict_model['__class__'] == 'BaseModel':
-                flag += 1
-            self.assertTrue(flag == 1)
-        for key, value in my_dict_model.items():
-            if key == 'created_at':
-                self.assertIsInstance(value, str)
-            if key == 'updated_at':
-                self.assertIsInstance(value, str)
+        """Test the to_dict method from BaseModel"""
+        bm1 = BaseModel()
+        bm1_dict = bm1.to_dict()
+        self.assertIsInstance(bm1_dict, dict)
+        self.assertEqual(bm1_dict["__class__"], "BaseModel")
+        self.assertEqual(str(bm1.id), bm1_dict["id"])
+        self.assertIsInstance(bm1_dict["created_at"], str)
+        self.assertIsInstance(bm1_dict["updated_at"], str)
 
-    def test_kwargs(self):
-        '''check when a dictionary in sent as **kwargs argument'''
-        self.my_object.name = "Holberton"
-        self.my_object.my_number = 89
-        my_object_json = self.my_object.to_dict()
-        my_object_kwargs = BaseModel(**my_object_json)
-        self.assertNotEqual(my_object_kwargs, self.my_object)
+    def test_save(self):
+        """Test to check each update in the storage"""
+        bm1 = BaseModel()
+        self.assertTrue(hasattr(bm1, "updated_at"))
+        bm1.save()
+        self.assertTrue(hasattr(bm1, "updated_at"))
+        t_arg = {'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579',
+                 'create_at': datetime(2017, 9, 28, 21, 5, 54, 119427),
+                 'updated_at': datetime(2017, 9, 28, 21, 5, 54, 119572),
+                 'name': 'bm1'}
+        bm2 = BaseModel(t_arg)
+        bm2.save()
+        last_time = bm2.updated_at
+        bm2.save()
+        self.assertNotEqual(last_time, bm2.updated_at)
+
+    def test_init_from_dict(self):
+        """test to check a new instance witk Kwargs"""
+        my_dict = {'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337',
+                   'created_at': '2017-09-28T21:03:54.052298',
+                   '__class__': 'BaseModel', 'my_number': 89,
+                   'updated_at': '2017-09-28T21:03:54.052302',
+                   'name': 'Holberton'}
+        bm1 = BaseModel(**my_dict)
+        self.assertIsInstance(bm1, BaseModel)
+        self.assertIsInstance(bm1.id, str)
+        self.assertEqual(bm1.id, '56d43177-cc5f-4d6c-a0c1-e167f8c27337')
+        self.assertIsInstance(bm1.created_at, datetime)
+        self.assertIsInstance(bm1.updated_at, datetime)
+        self.assertIsInstance(bm1.name, str)
+        self.assertEqual(bm1.name, 'Holberton')
+        self.assertEqual(
+            bm1.created_at.isoformat(), '2017-09-28T21:03:54.052298')
+        self.assertEqual(
+            bm1.updated_at.isoformat(), '2017-09-28T21:03:54.052302')
+
+    def test_new_attributte(self):
+        """test to check if new attribute  can be added"""
+        bm1 = BaseModel()
+        bm1.name = "Betty"
+        self.assertEqual(bm1.name, "Betty")
+
+if __name__ == '__main__':
+    unittest.main()
