3b1a1a52265622c5a0ec19f3eb854a653f153c59
Robel Pop <robelpopandro@gmail.com>
2022-10-29 12:30:33 +0300

    Documentation and Pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
-
+'''
+    Represents a  FileStorage class
+'''
 import json
 from models.base_model import BaseModel
 
 
 class FileStorage:
-
+    '''
+    Class that serializes instances to a JSON file and deserializes
+    JSON file to instances
+    '''
     __file_path = "file.json"
     __objects = {}
-    
-    def all(self):
 
+    def all(self):
+        '''
+        Method to return the dictionay __object
+        '''
         return self.__objects
 
     def new(self, obj):
-
+        '''
+        set a new object with the obj in the key
+        '''
         if obj:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
-
+        '''
+        Method to serializes __objects to the JSON file
+        '''
         dic_objects = {}
         for key, value in self.__objects.items():
             dic_objects[key] = value.to_dict()
@@ -28,11 +39,14 @@ class FileStorage:
             json.dump(dic_objects, f)
 
     def reload(self):
-        
+        '''
+        deserializes the JSON file to __objects
+        '''
         try:
             with open(self.__file_path, 'r') as f:
                 json_dict = json.load(f)
                 for key, value in json_dict.items():
-                    self.__objects[key] = eval(value['__class__'] + '(**value)')
+                    self.__objects[key] = eval(
+                        value['__class__'] + '(**value)')
         except FileNotFoundError:
             return
