fc97346caeabd4037d96eaa3225c03474b5f4c36
Robel Pop <robelpopandro@gmail.com>
2022-10-29 11:32:05 +0300

    Fix pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -29,29 +29,26 @@ class BaseModel:
                     continue
                 if key != "__class__":
                     setattr(self, key, value)
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now()
-                    models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
-                    def __str__(self):
-                        """ print a readable string """
-                        return "[{}] ({}) {}".format(
-                            self.__class__.__name__, self.id, self.__dict__)
+    def __str__(self):
+        """ print a readable string """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
-                    def save(self):
-                        """ updates with the current datetime """
-                        self.updated_at = datetime.now()
-                        models.storage.save()
+    def save(self):
+        """ updates with the current datetime """
+        self.updated_at = datetime.now()
 
-                        def to_dict(self):
-                            """ returns a dictionary with all keys/value
-                            of __dict__ of the instance """
-                            dictnew = copy.deepcopy(self.__dict__)
-                            dictnew['__class__'] = self.__class__.__name__
-
-                            formato = "%Y-%m-%dT%H:%M:%S.%f"
-                            dictnew['created_at'] = self.created_at.strftime(formato)
-                            dictnew['updated_at'] = self.updated_at.strftime(formato)
-                            dictnew['id'] = self.id
-                        return dictnew
+    def to_dict(self):
+        """ returns a dictionary with all keys/value
+        of __dict__ of the instance """
+        dictnew = copy.deepcopy(self.__dict__)
+        dictnew['__class__'] = self.__class__.__name__
+        formato = "%Y-%m-%dT%H:%M:%S.%f"
+        dictnew['created_at'] = self.created_at.strftime(formato)
+        dictnew['updated_at'] = self.updated_at.strftime(formato)
+        dictnew['id'] = self.id
+        return dictnew
